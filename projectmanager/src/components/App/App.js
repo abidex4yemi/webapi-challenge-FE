e51fa621cf4e0363bd693901e2e9096851270338
abidex4yemi <abidex4yemi@gmail.com>
2019-07-12 20:05:22 +0100

    Cosume get all projects endpoint

--- projectmanager/src/components/App/App.js
+++ projectmanager/src/components/App/App.js
@@ -1,12 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const BASE_URL = `https://web-api-sprint-1.herokuapp.com/api/v1`;
 
 const initialState = {
 	projects: [],
-	fetching: false
+	fetching: false,
+	error: {}
 };
 
 export const App = () => {
 	const [state, setState] = useState(initialState);
+
+	useEffect(() => {
+		const url = `${BASE_URL}/projects`;
+		getProjects(url);
+	}, []);
+
+	const getProjects = url => {
+		axios
+			.get(url)
+			.then(res => {
+				setState(prevState => ({
+					...prevState,
+					projects: res.data.body
+				}));
+			})
+			.catch(err => {
+				setState(prevState => ({
+					...prevState,
+					error: err
+				}));
+			});
+	};
 
 	return <div>Welcome</div>;
 };
