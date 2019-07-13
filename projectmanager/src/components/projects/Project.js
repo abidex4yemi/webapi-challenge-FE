46ff132f0fd37229f7c2deb92b65cd91b5be3460
abidex4yemi <abidex4yemi@gmail.com>
2019-07-13 12:50:48 +0100

    Create button component

--- projectmanager/src/components/projects/Project.js
+++ projectmanager/src/components/projects/Project.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Button } from '../~reuseables';
 
 export const Project = props => {
 	const { id, name, description } = props;
 
 	return (
 		<div>
-			<h3>Name: {name}</h3>
-			<p>Description: {description}</p>
+			<Link to={`/projects/${id}`}>
+				<h3>Name: {name}</h3>
+				<p>Description: {description}</p>
+			</Link>
+
+			<div>
+				<Button>Delete</Button>
+				<Button>Update</Button>
+			</div>
 		</div>
 	);
 };
---
+++ projectmanager/src/components/~reuseables/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+export const Button = props => {
+	const { children, onClick, type } = props;
+
+	return (
+		<StyledButton type={type} onClick={onClick}>
+			{children}
+		</StyledButton>
+	);
+};
+
+Button.propTypes = {
+	onClick: PropTypes.func.isRequired,
+	type: PropTypes.string
+};
+
+Button.defaultProps = {
+	type: 'text'
+};
+
+const StyledButton = styled.button`
+	font-size: 16px;
+	width: 120px;
+	height: 40px;
+	border: 0;
+	background: #79589f;
+	color: #fff;
+	border-radius: 5px;
+`;
