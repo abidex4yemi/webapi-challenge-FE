a574b5a510a5bd841c257258156eb230eff85053
abidex4yemi <abidex4yemi@gmail.com>
2019-07-13 13:03:11 +0100

    Style project component

--- projectmanager/src/components/projects/Projects.js
+++ projectmanager/src/components/projects/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { ProjectList } from './ProjectList';
 
 export const Projects = props => {
@@ -7,17 +8,17 @@ export const Projects = props => {
 
 	return (
 		<React.Fragment>
-			<header>
-				<div>
+			<StyledHeader>
+				<Container>
 					<h1>Welcome to project manager app</h1>
-				</div>
-			</header>
+				</Container>
+			</StyledHeader>
 
 			<main>
-				<div>
+				<Container>
 					{fetching && <div>Loading...</div>}
 					{projects.length > 0 ? <ProjectList projects={projects} /> : <div>No project </div>}
-				</div>
+				</Container>
 			</main>
 		</React.Fragment>
 	);
@@ -27,3 +28,15 @@ Projects.propTypes = {
 	projects: PropTypes.array.isRequired,
 	fetching: PropTypes.bool.isRequired
 };
+
+const Container = styled.div`
+	width: 80%;
+	margin: 0 auto;
+`;
+
+const StyledHeader = styled.header`
+	background: #79589f;
+	color: #fff;
+	text-align: center;
+	padding: 1rem;
+`;
