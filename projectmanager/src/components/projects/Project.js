4106b23d645f9139268a0c7cf7e715a2cf7e3b50
abidex4yemi <abidex4yemi@gmail.com>
2019-07-13 13:37:58 +0100

    Style Project card component

--- projectmanager/src/components/projects/Project.js
+++ projectmanager/src/components/projects/Project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Button } from '../~reuseables';
 
@@ -7,17 +8,17 @@ export const Project = props => {
 	const { id, name, description } = props;
 
 	return (
-		<div>
+		<StyledProject>
 			<Link to={`/projects/${id}`}>
 				<h3>Name: {name}</h3>
 				<p>Description: {description}</p>
 			</Link>
 
-			<div>
+			<StyledButtonContainer>
 				<Button>Delete</Button>
 				<Button>Update</Button>
-			</div>
-		</div>
+			</StyledButtonContainer>
+		</StyledProject>
 	);
 };
 
@@ -26,3 +27,34 @@ Project.propTypes = {
 	name: PropTypes.string.isRequired,
 	description: PropTypes.string.isRequired
 };
+
+const StyledProject = styled.div`
+	background: #f4f4f4;
+	padding: 20px;
+	height: 200px;
+	width: 250px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	text-align: center;
+	border-radius: 5px;
+
+	a {
+		text-decoration: none;
+		color: #5f467e;
+
+		&:hover {
+			color: purple;
+		}
+	}
+
+	h3,
+	p {
+		margin-bottom: 16px;
+	}
+`;
+
+const StyledButtonContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
